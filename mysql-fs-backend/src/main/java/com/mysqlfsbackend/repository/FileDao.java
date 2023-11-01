ee918eb2f089e594dfd40636bb5cf2d42bd68208
Max <80323354+GuyBritish@users.noreply.github.com>
2023-11-01 09:47:35 -0500

    Add custom file queries

--- mysql-fs-backend/src/main/java/com/mysqlfsbackend/repository/FileDao.java
+++ mysql-fs-backend/src/main/java/com/mysqlfsbackend/repository/FileDao.java
@@ -2,10 +2,15 @@ package com.mysqlfsbackend.repository;
 
 import com.mysqlfsbackend.model.FileEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface FileDao extends JpaRepository<FileEntity, UUID> {
+    @Query(value = "SELECT * FROM File AS f WHERE f.parentDirId = :parentDirId AND f.name = :name",
+            nativeQuery = true)
+    FileEntity getChildByName(@Param("parentDirId") UUID parentDirId, @Param("name") String name);
 }
