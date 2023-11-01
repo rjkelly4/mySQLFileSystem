b4f3e466a8adfff103bd3f1393527e3764e3109b
Max <80323354+GuyBritish@users.noreply.github.com>
2023-11-01 09:47:17 -0500

    Add custom directory queries

--- mysql-fs-backend/src/main/java/com/mysqlfsbackend/repository/DirectoryDao.java
+++ mysql-fs-backend/src/main/java/com/mysqlfsbackend/repository/DirectoryDao.java
@@ -2,10 +2,19 @@ package com.mysqlfsbackend.repository;
 
 import com.mysqlfsbackend.model.DirectoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
 public interface DirectoryDao extends JpaRepository<DirectoryEntity, UUID> {
+    @Query(value = "SELECT * FROM Directory AS d WHERE d.parentDirId IS NULL AND d.name = 'root'",
+            nativeQuery = true)
+    DirectoryEntity getRoot();
+
+    @Query(value = "SELECT * FROM Directory AS d WHERE d.parentDirId = :parentDirId AND d.name = :name",
+            nativeQuery = true)
+    DirectoryEntity getChildByName(@Param("parentDirId") UUID parentDirId, @Param("name") String name);
 }
