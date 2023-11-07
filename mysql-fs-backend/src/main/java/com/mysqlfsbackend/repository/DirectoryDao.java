c8507e3112b9a67a3a612151885e7be7ac85bcbd
Alex Mezera <amezera@wisc.edu>
2023-11-07 17:55:18 -0600

    Add method queries

--- mysql-fs-backend/src/main/java/com/mysqlfsbackend/repository/DirectoryDao.java
+++ mysql-fs-backend/src/main/java/com/mysqlfsbackend/repository/DirectoryDao.java
@@ -17,4 +17,19 @@ public interface DirectoryDao extends JpaRepository<DirectoryEntity, UUID> {
     @Query(value = "SELECT * FROM Directory AS d WHERE d.parentDirId = :parentDirId AND d.name = :name",
             nativeQuery = true)
     DirectoryEntity getChildByName(@Param("parentDirId") UUID parentDirId, @Param("name") String name);
+
+    @Query(value = "DELETE FROM Directory WHERE d.id = :id AND d.name = :name")
+    void delete(@Param("id") UUID id, @Param("name") String name);
+
+    @Query(value = "UPDATE directories "
+     + "SET d.name = :name, d.parent_dir = :parentDirId, d.permission = :permission, d.owner_user_id = :ownerUserId, "
+     + "d.owner_group_id = ownerGroupId "
+     + "where d.id = ?")
+    UUID put(@Param("name") String name, @Param("parentDirId") UUID parentDirId, @Param("permission") int permission,
+                @Param("ownerUserID") int ownerUserId, @Param("ownerGroupID") int ownerGroupId);
+
+    @Query(value = "INSERT INTO directories (name, parent_dir, permission, owner_user_id, owner_group_id) " + 
+        "VALUES (:name, :parentDirId, :permission, :ownerUserId, :ownerGroupId);")
+    UUID post(@Param("name") String name, @Param("parentDirId") UUID parentDirId, @Param("permission") int permission,
+                @Param("ownerUserID") int ownerUserId, @Param("ownerGroupID") int ownerGroupId);
 }
--- mysql-fs-backend/src/main/java/com/mysqlfsbackend/repository/FileDao.java
+++ mysql-fs-backend/src/main/java/com/mysqlfsbackend/repository/FileDao.java
@@ -13,4 +13,23 @@ public interface FileDao extends JpaRepository<FileEntity, UUID> {
     @Query(value = "SELECT * FROM File AS f WHERE f.parentDirId = :parentDirId AND f.name = :name",
             nativeQuery = true)
     FileEntity getChildByName(@Param("parentDirId") UUID parentDirId, @Param("name") String name);
+
+    @Query (value = "SELECT * FROM File AS f WHERE f.id = :id")
+    FileEntity getChildById(@Param("id") UUID id);
+
+
+    @Query (value = "DELETE FROM File WHERE f.id = :id AND f.name = :name")
+    void delete(@Param("id") UUID id, @Param("name") String name);
+
+    @Query(value = "UPDATE File "
+            + "SET f.name = :name, f.parent_dir = :parentDirId, f.permission = :permission, f.owner_user_id = :ownerUserId, "
+            + "f.owner_group_id = ownerGroupId "
+            + "where f.id = ?")
+    UUID put(@Param("name") String name, @Param("parentDirId") UUID parentDirId, @Param("permission") int permission,
+                @Param("ownerUserID") int ownerUserId, @Param("ownerGroupID") int ownerGroupId);
+
+    @Query(value = "INSERT INTO File (name, parent_dir, permission, owner_user_id, owner_group_id) " + 
+        "VALUES (:name, :parentDirId, :permission, :ownerUserId, :ownerGroupId);")
+    UUID post(@Param("name") String name, @Param("parentDirId") UUID parentDirId, @Param("permission") int permission,
+                @Param("ownerUserID") int ownerUserId, @Param("ownerGroupID") int ownerGroupId);
 }
