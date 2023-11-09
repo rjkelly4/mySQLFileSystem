a5aad5a9823ae148e410137fc701f2cdd57d534f
Max <80323354+GuyBritish@users.noreply.github.com>
2023-11-09 09:42:38 -0600

    Update layer browsing query and id type

--- mysql-fs-backend/src/main/java/com/mysqlfsbackend/repository/DirectoryDao.java
+++ mysql-fs-backend/src/main/java/com/mysqlfsbackend/repository/DirectoryDao.java
@@ -1,20 +1,24 @@
 package com.mysqlfsbackend.repository;
 
-import com.mysqlfsbackend.model.DirectoryEntity;
+import com.mysqlfsbackend.model.filesystem.DirectoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
+import java.util.List;
 
 @Repository
-public interface DirectoryDao extends JpaRepository<DirectoryEntity, UUID> {
+public interface DirectoryDao extends JpaRepository<DirectoryEntity, String> {
     @Query(value = "SELECT * FROM Directory AS d WHERE d.parentDirId IS NULL AND d.name = 'root'",
             nativeQuery = true)
     DirectoryEntity getRoot();
 
     @Query(value = "SELECT * FROM Directory AS d WHERE d.parentDirId = :parentDirId AND d.name = :name",
             nativeQuery = true)
-    DirectoryEntity getChildByName(@Param("parentDirId") UUID parentDirId, @Param("name") String name);
+    DirectoryEntity getChildByName(@Param("parentDirId") String parentDirId, @Param("name") String name);
+
+    @Query(value = "SELECT * FROM Directory AS d WHERE d.parentDirId IN :parentDirIds ORDER BY d.parentDirId",
+            nativeQuery = true)
+    List<DirectoryEntity> getOrderedChildLayer(@Param("parentDirIds") List<String> parentDirIds);
 }
--- mysql-fs-backend/src/main/java/com/mysqlfsbackend/repository/FileDao.java
+++ mysql-fs-backend/src/main/java/com/mysqlfsbackend/repository/FileDao.java
@@ -1,16 +1,20 @@
 package com.mysqlfsbackend.repository;
 
-import com.mysqlfsbackend.model.FileEntity;
+import com.mysqlfsbackend.model.filesystem.FileEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
+import java.util.List;
 
 @Repository
-public interface FileDao extends JpaRepository<FileEntity, UUID> {
+public interface FileDao extends JpaRepository<FileEntity, String> {
     @Query(value = "SELECT * FROM File AS f WHERE f.parentDirId = :parentDirId AND f.name = :name",
             nativeQuery = true)
-    FileEntity getChildByName(@Param("parentDirId") UUID parentDirId, @Param("name") String name);
+    FileEntity getChildByName(@Param("parentDirId") String parentDirId, @Param("name") String name);
+
+    @Query(value = "SELECT * FROM File AS f WHERE f.parentDirId IN :parentDirIds ORDER BY f.parentDirId",
+            nativeQuery = true)
+    List<FileEntity> getOrderedChildLayer(@Param("parentDirIds") List<String> parentDirIds);
 }
