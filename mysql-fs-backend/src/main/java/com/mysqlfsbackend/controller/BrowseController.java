8c8052c017c23b08a09b72a31074e997bd1bf3da
Max <80323354+GuyBritish@users.noreply.github.com>
2023-11-10 11:25:01 -0600

    Add ResponseEntity

--- mysql-fs-backend/src/main/java/com/mysqlfsbackend/controller/BrowseController.java
+++ mysql-fs-backend/src/main/java/com/mysqlfsbackend/controller/BrowseController.java
@@ -1,17 +1,21 @@
 package com.mysqlfsbackend.controller;
 
-import com.mysqlfsbackend.model.dto.BrowseRequestBody;
+import com.mysqlfsbackend.model.dto.filesystem.DirectoryDto;
+import com.mysqlfsbackend.model.dto.filesystem.FileSystemObjectDtoFactory;
+import com.mysqlfsbackend.model.dto.filesystem.FileDto;
+import com.mysqlfsbackend.model.dto.http.BrowseRequestBody;
 import com.mysqlfsbackend.model.filesystem.DirectoryEntity;
 import com.mysqlfsbackend.model.filesystem.FileEntity;
 import com.mysqlfsbackend.model.filesystem.FileSystemObject;
 import com.mysqlfsbackend.service.BrowseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -26,8 +30,8 @@ public class BrowseController {
     }
 
     @GetMapping(value = {"/api/browse/folders"})
-    public String getDirectoryContentFromPath(@RequestParam Optional<String> path,
-                          @RequestBody Optional<BrowseRequestBody> requestBody) {
+    public ResponseEntity<DirectoryDto> getDirectoryContentFromPath(@RequestParam Optional<String> path,
+                                                                    @RequestBody Optional<BrowseRequestBody> requestBody) {
         String browsePath = path.orElse("/");
         int depth = requestBody.isPresent() ? requestBody.get().getBrowseDepth() : 3;
 
@@ -38,17 +42,20 @@ public class BrowseController {
         Optional<DirectoryEntity> workDirectory = browseService.getDirectoryFromPath(root, directoryPath);
 
         List<List<FileSystemObject>> browseResults;
-        if (workDirectory.isPresent()) {
-            browseResults = browseService.browseDirectory(workDirectory.get(), depth);
+        if (workDirectory.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         } else {
-            browseResults = new ArrayList<>();
-        }
+            browseResults = browseService.browseDirectory(workDirectory.get(), depth);
 
-        return browseResults.toString();
+            return new ResponseEntity<>(
+                    FileSystemObjectDtoFactory.generateDirectoryDto(workDirectory.get(), browseResults),
+                    HttpStatus.OK
+            );
+        }
     }
 
     @GetMapping(value = {"/api/browse/files"})
-    public String getFileContentFromPath(@RequestParam String path) {
+    public ResponseEntity<FileDto> getFileContentFromPath(@RequestParam String path) {
         List<String> filePath = Arrays.asList(path.split("/"));
         DirectoryEntity root = browseService.getRoot();
 
@@ -57,9 +64,11 @@ public class BrowseController {
         Optional<FileEntity> targetFile = browseService.getFileFromPath(root, filePath);
 
         if (targetFile.isEmpty()) {
-            return "File not found";
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
 
-        return targetFile.get().getContent();
+        return new ResponseEntity<>((FileDto) FileSystemObjectDtoFactory.convertToDto(targetFile.get()),
+                HttpStatus.OK
+        );
     }
 }
