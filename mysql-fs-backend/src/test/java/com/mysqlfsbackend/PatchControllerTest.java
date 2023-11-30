8f8eadd5f9c6f95d9a36d6a53bff3e86f086021e
Alex Mezera <amezera@wisc.edu>
2023-11-30 13:47:59 -0600

    Add apiController test

---
+++ mysql-fs-backend/src/test/java/com/mysqlfsbackend/PatchControllerTest.java
@@ -0,0 +1,31 @@
+package com.mysqlfsbackend;
+
+
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class PatchControllerTest {
+
+    @Autowired private DataSource dataSource;
+    @Autowired private EntityManager entityManager;
+    @Autowired private UserRepository userRepository;
+
+    @Test
+    void injectedComponentsAreNotNull(){
+        assertThat(dataSource).isNotNull();
+        assertThat(jdbcTemplate).isNotNull();
+        assertThat(entityManager).isNotNull();
+        assertThat(userRepository).isNotNull();
+    }
+
+}
