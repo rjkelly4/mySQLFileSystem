ea160e0ca85453b8f395cf944ed102cb0ab278e9
Khai Bui <kmbui2@wisc.edu>
2023-12-06 15:46:22 -0600

    Database design update

--- docs/DatabaseDesign.md
+++ docs/DatabaseDesign.md
@@ -18,9 +18,11 @@ design as the team works on implementation.
 -   #### Entities
     -   User
     -   Group
+    -   UserThirdParty
     -   Directory
     -   File
 -   #### Relationships
+    -   UserThirdParty <-> User
     -   Group <- User
     -   User <- Directory
     -   UserGroup <- Directory
@@ -30,12 +32,38 @@ design as the team works on implementation.
     -   Directory <-> Directory (parent)
 -   #### Constraints
     -   Foreign key `Directory.parentDirId` and `File.parentDirId` -> `Directory.id`
-    -   Foreign key `Directory.ownerUserId` and `File.ownerUserId` -> `User.id`
+    -   Foreign key `UserThirdParty.userId`, `Directory.ownerUserId`, and `File.ownerUserId` -> `User.id`
     -   Foreign key `Directory.ownerGroupId` and `File.ownerGroupId` -> `Group.id`
+    -   Foreign key `UserThirdParty.userId` 
 -   #### Data Schema
     -   See ER Diagram
 
 ### MySQL Schema
+
+#### UserGroup
+
+| Field        | Type         | Null | Key | Default                                             |
+| ------------ | ------------ | ---- | --- | --------------------------------------------------- |
+| id           | binary(36)   | No   | PRI | uuid                                                |
+| name         | varchar(255) | No   |     |                                                     |
+
+#### User
+
+| Field        | Type         | Null | Key | Default                                             |
+| ------------ | ------------ | ---- | --- | --------------------------------------------------- |
+| id           | binary(36)   | No   | PRI | uuid                                                |
+| name         | varchar(255) | No   |     |                                                     |
+| email        | varchar(255) | No   |     | unique, follows email format                        |
+| password     | varchar(255) | No   |     | hashed                                              |
+| groupId      | binary(36)   | No   | MUL | uuid                                                |
+
+#### UserThirdParty
+
+| Field        | Type         | Null | Key | Default                                             |
+| ------------ | ------------ | ---- | --- | --------------------------------------------------- |
+| thirdPartyId | binary(36)   | No   | PRI | uuid                                                |
+| userId       | binary(36)   | No   | UNI | uuid                                                |
+
 
 #### Directory
 
@@ -44,9 +72,9 @@ design as the team works on implementation.
 | id           | binary(36)   | No   | PRI | uuid                                                |
 | name         | varchar(255) | No   |     | unique accross files and dirs within a path         |
 | parentDirId  | binary(36)   | Yes  | MUL | uuid                                                |
-| permission   | int          | Yes  |     | non-negative, at most 3 digits, each digit in 0 - 7 |
-| ownerUserId  | int          | No   |     |                                                     |
-| ownerGroupId | int          | No   |     |                                                     |
+| permission   | varchar(255) | No   |     | non-negative, at most 3 digits, each digit in 0 - 7 |
+| ownerUserId  | binary(36)   | No   | MUL | uuid                                                |
+| ownerGroupId | binary(36)   | No   | MUL | uuid                                                |
 | size         | bigint       | No   |     | non-negative, in byte(s)                            |
 
 #### File
@@ -56,9 +84,9 @@ design as the team works on implementation.
 | id           | binary(36)          | No   | PRI | uuid                                                |
 | name         | varchar(255)        | No   |     | unique accross files and dirs within a path         |
 | parentDirId  | binary(36)          | Yes  | MUL | uuid                                                |
-| permission   | int                 | Yes  |     | non-negative, at most 3 digits, each digit in 0 - 7 |
-| ownerUserId  | int                 | No   |     |                                                     |
-| ownerGroupId | int                 | No   |     |                                                     |
+| permission   | varchar(255)        | No   |     | non-negative, at most 3 digits, each digit in 0 - 7 |
+| ownerUserId  | binary(36)          | No   | MUL | uuid                                                |
+| ownerGroupId | binary(36)          | No   | MUL | uuid                                                |
 | size         | bigint              | No   |     | non-negative, in byte(s)                            |
 | fileType     | enum('NONE', 'TXT') | No   |     |                                                     |
 | content      | varchar(255)        | Yes  |     |                                                     |
@@ -66,19 +94,48 @@ design as the team works on implementation.
 ### MySQL Commands
 
 ```sql
+CREATE TABLE UserGroup
+(
+    id BINARY(36) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+```
+
+```sql
+CREATE TABLE User
+(
+    id BINARY(36) PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
+        CHECK (email REGEXP '^[a-z0-9!#$%&''*+/=?^_`{|}~-]+(\.[a-z0-9!#$%&''*+/=?^_`{|}~-]+)*@([a-z0-9]+[a-z0-9-]*)*[a-z0-9]+(\.([a-z0-9]+[a-z0-9-]*)*[a-z0-9]+)*\.[a-z]{2,6}$'),
+    password VARCHAR(255) NOT NULL,
+    groupId BINARY(36),
+    FOREIGN KEY (groupId) REFERENCES UserGroup(id)
+);
+```
+
+```sql
+CREATE TABLE UserThirdParty (
+    thirdPartyId VARCHAR(255) PRIMARY KEY,
+    userId BINARY(36) UNIQUE NOT NULL,
+    FOREIGN KEY (userId) REFERENCES User(id),
+    INDEX (userId)
+);
+```
+
+```sql
 CREATE TABLE Directory
 (
     id BINARY(36) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     parentDirId BINARY(36),
-    permission INT CHECK (
-        permission >= 0 AND permission <= 777 AND
-        CAST(permission AS CHAR(3)) REGEXP '^[0-7]{1,3}$'
-    ),
-    ownerUserId INT NOT NULL,
-    ownerGroupId INT NOT NULL,
+    permission VARCHAR(3) NOT NULL CHECK (permission REGEXP '^[0-7][0-7][0-7]$'),
+    ownerUserId BINARY(36) NOT NULL,
+    ownerGroupId BINARY(36) NOT NULL,  # TODO: Learn how group works
     size BIGINT CHECK (size >= 0) NOT NULL,
-    FOREIGN KEY (parentDirId) REFERENCES Directory(id)
+    FOREIGN KEY (parentDirId) REFERENCES Directory(id),
+    FOREIGN KEY (ownerUserId) REFERENCES User(id),
+    FOREIGN KEY (ownerGroupId) REFERENCES UserGroup(id)
 );
 ```
 
@@ -88,16 +145,15 @@ CREATE TABLE File
     id BINARY(36) PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     parentDirId BINARY(36),
-    permission INT CHECK (
-        permission >= 0 AND permission <= 777 AND
-        CAST(permission AS CHAR(3)) REGEXP '^[0-7]{1,3}$'
-    ),
-    ownerUserId INT NOT NULL,
-    ownerGroupId INT NOT NULL ,
+    permission VARCHAR(3) NOT NULL CHECK (permission REGEXP '^[0-7][0-7][0-7]$'),
+    ownerUserId BINARY(36) NOT NULL,
+    ownerGroupId BINARY(36) NOT NULL,
     size BIGINT CHECK (size >= 0) NOT NULL,
     fileType ENUM('NONE', 'TXT') NOT NULL,
     content VARCHAR(255),
-    FOREIGN KEY (parentDirId) REFERENCES Directory(id)
+    FOREIGN KEY (parentDirId) REFERENCES Directory(id),
+    FOREIGN KEY (ownerUserId) REFERENCES User(id),
+    FOREIGN KEY (ownerGroupId) REFERENCES UserGroup(id)
 );
 ```
 
--- utilities/sample_fs.sql
+++ utilities/sample_fs.sql
@@ -40,67 +40,99 @@
 # 			secret
 # 		/Misc3 0
 
-INSERT INTO Directory
-VALUES ('57d201cf-7f08-11ee-a0eb-0242ac1b0003', 'root', NULL, '000', 1, 1, 19);
+INSERT INTO UserGroup
+VALUES ('4e935332-8fd5-11ee-9fdd-0242ac120003', 'root');
+
+INSERT INTO User
+VALUES ('0a5e53ea-8fd7-11ee-9fdd-0242ac120003', 'root', 'kmbui2@wisc.edu', 'notguest', '4e935332-8fd5-11ee-9fdd-0242ac120003');
 
 INSERT INTO Directory
-VALUES ('93f0cac9-7f08-11ee-a0eb-0242ac1b0003', 'companyA', '57d201cf-7f08-11ee-a0eb-0242ac1b0003', '777', 1, 1, 10);
-INSERT INTO Directory
-VALUES ('9427a8a3-7f08-11ee-a0eb-0242ac1b0003', 'companyB', '57d201cf-7f08-11ee-a0eb-0242ac1b0003', '777', 1, 1, 9);
+VALUES ('57d201cf-7f08-11ee-a0eb-0242ac1b0003', 'root', NULL, '000', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 19);
 
 INSERT INTO Directory
-VALUES ('d43aa901-7f08-11ee-a0eb-0242ac1b0003', 'Staff', '93f0cac9-7f08-11ee-a0eb-0242ac1b0003', '777', 1, 1, 6);
+VALUES ('93f0cac9-7f08-11ee-a0eb-0242ac1b0003', 'companyA', '57d201cf-7f08-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 10);
 INSERT INTO Directory
-VALUES ('d46e2d39-7f08-11ee-a0eb-0242ac1b0003', 'Staff', '9427a8a3-7f08-11ee-a0eb-0242ac1b0003', '777', 1, 1, 7);
-INSERT INTO Directory
-VALUES ('d49eeb08-7f08-11ee-a0eb-0242ac1b0003', 'Clients', '93f0cac9-7f08-11ee-a0eb-0242ac1b0003', '777', 1, 1, 4);
-INSERT INTO Directory
-VALUES ('d4cfe7e3-7f08-11ee-a0eb-0242ac1b0003', 'Clients', '9427a8a3-7f08-11ee-a0eb-0242ac1b0003', '777', 1, 1, 1);
-INSERT INTO Directory
-VALUES (UUID(), 'Misc1', '9427a8a3-7f08-11ee-a0eb-0242ac1b0003', '777', 1, 1, 0);
-INSERT INTO Directory
-VALUES (UUID(), 'Misc2', '9427a8a3-7f08-11ee-a0eb-0242ac1b0003', '777', 1, 1, 1);
-INSERT INTO Directory
-VALUES (UUID(), 'Misc3', '9427a8a3-7f08-11ee-a0eb-0242ac1b0003', '777', 1, 1, 0);
+VALUES ('9427a8a3-7f08-11ee-a0eb-0242ac1b0003', 'companyB', '57d201cf-7f08-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 9);
 
 INSERT INTO Directory
-VALUES ('2cb9136c-7f09-11ee-a0eb-0242ac1b0003', 'Employees', 'd43aa901-7f08-11ee-a0eb-0242ac1b0003', '777', 1, 1, 5);
+VALUES ('d43aa901-7f08-11ee-a0eb-0242ac1b0003', 'Staff', '93f0cac9-7f08-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 6);
 INSERT INTO Directory
-VALUES (UUID(), 'Managers', 'd43aa901-7f08-11ee-a0eb-0242ac1b0003', '777', 1, 1, 1);
+VALUES ('d46e2d39-7f08-11ee-a0eb-0242ac1b0003', 'Staff', '9427a8a3-7f08-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 7);
 INSERT INTO Directory
-VALUES ('2d0c84b3-7f09-11ee-a0eb-0242ac1b0003', 'Employees', 'd46e2d39-7f08-11ee-a0eb-0242ac1b0003', '777', 1, 1, 3);
+VALUES ('d49eeb08-7f08-11ee-a0eb-0242ac1b0003', 'Clients', '93f0cac9-7f08-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 4);
 INSERT INTO Directory
-VALUES (UUID(), 'Managers', 'd46e2d39-7f08-11ee-a0eb-0242ac1b0003', '777', 1, 1, 4);
+VALUES ('d4cfe7e3-7f08-11ee-a0eb-0242ac1b0003', 'Clients', '9427a8a3-7f08-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1);
 INSERT INTO Directory
-VALUES (UUID(), 'History', 'd49eeb08-7f08-11ee-a0eb-0242ac1b0003', '777', 1, 1, 1);
+VALUES (UUID(), 'Misc1', '9427a8a3-7f08-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 0);
 INSERT INTO Directory
-VALUES (UUID(), 'History', 'd4cfe7e3-7f08-11ee-a0eb-0242ac1b0003', '777', 1, 1, 1);
+VALUES ('98898faf-835c-11ee-9fdd-0242ac120003', 'Misc2', '9427a8a3-7f08-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1);
+INSERT INTO Directory
+VALUES (UUID(), 'Misc3', '9427a8a3-7f08-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 0);
 
 INSERT INTO Directory
-VALUES ('eb83eba4-7f09-11ee-a0eb-0242ac1b0003', 'Team1', '2cb9136c-7f09-11ee-a0eb-0242ac1b0003', '777', 1, 1, 2);
+VALUES ('2cb9136c-7f09-11ee-a0eb-0242ac1b0003', 'Employees', 'd43aa901-7f08-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 5);
 INSERT INTO Directory
-VALUES ('eba0de0b-7f09-11ee-a0eb-0242ac1b0003', 'Team2', '2cb9136c-7f09-11ee-a0eb-0242ac1b0003', '777', 1, 1, 1);
+VALUES ('994af418-835c-11ee-9fdd-0242ac120003', 'Managers', 'd43aa901-7f08-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1);
 INSERT INTO Directory
-VALUES (UUID(), 'Team1', '2d0c84b3-7f09-11ee-a0eb-0242ac1b0003', '777', 1, 1, 0);
+VALUES ('2d0c84b3-7f09-11ee-a0eb-0242ac1b0003', 'Employees', 'd46e2d39-7f08-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 3);
 INSERT INTO Directory
-VALUES ('ebde4de7-7f09-11ee-a0eb-0242ac1b0003', 'Team2', '2d0c84b3-7f09-11ee-a0eb-0242ac1b0003', '777', 1, 1, 1);
+VALUES ('99eca125-835c-11ee-9fdd-0242ac120003', 'Managers', 'd46e2d39-7f08-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 4);
+INSERT INTO Directory
+VALUES ('9a2b4f75-835c-11ee-9fdd-0242ac120003', 'History', 'd49eeb08-7f08-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1);
+INSERT INTO Directory
+VALUES ('9a6a5397-835c-11ee-9fdd-0242ac120003', 'History', 'd4cfe7e3-7f08-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1);
+
+INSERT INTO Directory
+VALUES ('eb83eba4-7f09-11ee-a0eb-0242ac1b0003', 'Team1', '2cb9136c-7f09-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 2);
+INSERT INTO Directory
+VALUES ('eba0de0b-7f09-11ee-a0eb-0242ac1b0003', 'Team2', '2cb9136c-7f09-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1);
+INSERT INTO Directory
+VALUES (UUID(), 'Team1', '2d0c84b3-7f09-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 0);
+INSERT INTO Directory
+VALUES ('ebde4de7-7f09-11ee-a0eb-0242ac1b0003', 'Team2', '2d0c84b3-7f09-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1);
 
 INSERT INTO File
-VALUES (UUID(), 'e1', 'eb83eba4-7f09-11ee-a0eb-0242ac1b0003', '777', 1, 1, 1, 1, 'A employee 1');
+VALUES (UUID(), 'e1', 'eb83eba4-7f09-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1, 1, 'A employee 1');
 INSERT INTO File
-VALUES (UUID(), 'e2', 'eb83eba4-7f09-11ee-a0eb-0242ac1b0003', '777', 1, 1, 1, 1, 'A employee 2');
+VALUES (UUID(), 'e2', 'eb83eba4-7f09-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1, 1, 'A employee 2');
 
 INSERT INTO File
-VALUES (UUID(), 'e3', 'eba0de0b-7f09-11ee-a0eb-0242ac1b0003', '777', 1, 1, 1, 1, 'A employee 3');
+VALUES (UUID(), 'e3', 'eba0de0b-7f09-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1, 1, 'A employee 3');
 
 INSERT INTO File
-VALUES (UUID(), 'e1', 'ebde4de7-7f09-11ee-a0eb-0242ac1b0003', '777', 1, 1, 1, 1, 'B employee 1');
+VALUES (UUID(), 'e1', 'ebde4de7-7f09-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1, 1, 'B employee 1');
 
 INSERT INTO File
-VALUES (UUID(), 'duties', '2cb9136c-7f09-11ee-a0eb-0242ac1b0003', '777', 1, 1, 1, 1, 'A employee duties');
+VALUES (UUID(), 'duties', '2cb9136c-7f09-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1, 1, 'A employee duties');
 INSERT INTO File
-VALUES (UUID(), 'schedule', '2cb9136c-7f09-11ee-a0eb-0242ac1b0003', '777', 1, 1, 1, 1, 'A employee schedule');
+VALUES (UUID(), 'schedule', '2cb9136c-7f09-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1, 1, 'A employee schedule');
 INSERT INTO File
-VALUES (UUID(), 'duties', '2d0c84b3-7f09-11ee-a0eb-0242ac1b0003', '777', 1, 1, 1, 1, 'B employee duties');
+VALUES (UUID(), 'duties', '2d0c84b3-7f09-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1, 1, 'B employee duties');
 INSERT INTO File
-VALUES (UUID(), 'schedule', '2d0c84b3-7f09-11ee-a0eb-0242ac1b0003', '777', 1, 1, 1, 1, 'B employee schedule');
+VALUES (UUID(), 'schedule', '2d0c84b3-7f09-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1, 1, 'B employee schedule');
+
+INSERT INTO File
+VALUES (UUID(), 'm1', '994af418-835c-11ee-9fdd-0242ac120003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1, 1, 'A manager 1');
+INSERT INTO File
+VALUES (UUID(), 'm1', '99eca125-835c-11ee-9fdd-0242ac120003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1, 1, 'B manager 1');
+INSERT INTO File
+VALUES (UUID(), 'm2', '99eca125-835c-11ee-9fdd-0242ac120003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1, 1, 'B manager 2');
+INSERT INTO File
+VALUES (UUID(), 'm3', '99eca125-835c-11ee-9fdd-0242ac120003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1, 1, 'B manager 3');
+INSERT INTO File
+VALUES (UUID(), 'm4', '99eca125-835c-11ee-9fdd-0242ac120003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1, 1, 'B manager 4');
+
+INSERT INTO File
+VALUES (UUID(), 'clientA', 'd49eeb08-7f08-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1, 1, 'A clientA');
+INSERT INTO File
+VALUES (UUID(), 'clientB', 'd49eeb08-7f08-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1, 1, 'A clientB');
+INSERT INTO File
+VALUES (UUID(), 'clientC', 'd49eeb08-7f08-11ee-a0eb-0242ac1b0003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1, 1, 'A clientC');
+
+INSERT INTO File
+VALUES (UUID(), 'history', '9a2b4f75-835c-11ee-9fdd-0242ac120003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1, 1, 'A history');
+INSERT INTO File
+VALUES (UUID(), 'history', '9a6a5397-835c-11ee-9fdd-0242ac120003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1, 1, 'B history');
+
+INSERT INTO File
+VALUES (UUID(), 'secret', '98898faf-835c-11ee-9fdd-0242ac120003', '777', '0a5e53ea-8fd7-11ee-9fdd-0242ac120003', '4e935332-8fd5-11ee-9fdd-0242ac120003', 1, 1, 'Admin secret');
